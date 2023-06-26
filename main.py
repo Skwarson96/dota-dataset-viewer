76e6f2076e3025a009ddb1fbc6e21433daf2928b
Skwarson96 <maciej.skwara2@gmail.com>
2023-06-26 23:43:18 +0200

    add drawing frames

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import argparse
 import sys
 import cv2
 import os
+import numpy as np
 from PIL import Image
 from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QPushButton, QVBoxLayout, QHBoxLayout, QWidget, QSlider, QFrame, QGroupBox, QGridLayout, QGraphicsView, QScrollArea
 from PyQt5.QtGui import QPixmap, QWheelEvent, QImage
@@ -10,10 +11,7 @@ from PyQt5.QtCore import Qt
 
 class ImageProcessor:
     def __init__(self):
-        # self.image_path = image_path
         self.image = None
-
-        # self.read_images_names(image_path)
 
     def read_images_names(self, image_path):
         image_names = []
@@ -27,15 +25,31 @@ class ImageProcessor:
         self.image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
 
-    # def read_annotation_files(self, annotations_path):
-    #     annotations_files = []
-    #     for file_name in os.listdir(annotations_path):
-    #         if file_name.endswith('.jpg') or file_name.endswith('.png'):
-    #             annotations_files.append(file_name)
+    def read_annotation_files(self, annotations_path):
+        annotations_files = []
+        for file_name in os.listdir(annotations_path):
+            if file_name.endswith('.txt'):
+                annotations_files.append(file_name)
+        return annotations_files
 
+    def read_and_draw_annotations(self, annotation_path):
+        with open(annotation_path, 'r') as file:
+            for line in file:
+                line = str.split(line, ' ')
+                if len(line) == 10:
+                    int_values = []
+                    for value in line[0:8]:
+                        value = float(value)
+                        value = int(value)
+                        int_values.append(value)
 
-    def draw_annotation(self, points, color):
-        pass
+                    x_1, y_1, x_2, y_2, x_3, y_3, x_4, y_4 = int_values
+                    points = [(x_1, y_1), (x_2, y_2), (x_3, y_3), (x_4, y_4)]
+                    category, difficult = line[8:10]
+
+                    points = np.array(points, dtype=np.int32)
+
+                    cv2.polylines(self.image, [points], isClosed=True, color=(0, 255, 0), thickness=2)
 
     def save_image(self, output_path):
         self.image.save(output_path)
@@ -49,13 +63,21 @@ class ImageProcessor:
 
 
 class ImageViewer(QMainWindow):
-    def __init__(self, images_path):
+    def __init__(self, images_path, annotations_path):
         super().__init__()
         self.images_path = images_path
+        self.annotations_path = annotations_path
         self.setWindowTitle("Test window")
 
         self.current_img_index = 0
         self.image_processor = ImageProcessor()
+
+        self.images_names = self.image_processor.read_images_names(self.images_path)
+        self.annotations_files_names = self.image_processor.read_annotation_files(self.annotations_path)
+
+        if len(self.images_names) != len(self.annotations_files_names):
+            print('Different length of lists with filenames, check the number of files in the given folders')
+            exit()
 
         self.window_width = 800
         self.window_height = 800
@@ -83,9 +105,11 @@ class ImageViewer(QMainWindow):
         self.image_label.setAlignment(Qt.AlignCenter)
 
     def show_image(self):
-        self.images_names = self.image_processor.read_images_names(self.images_path)
 
         self.image_processor.read_image(self.images_path + '/' + self.images_names[self.current_img_index])
+
+        annotation_file_name = self.images_names[self.current_img_index].rsplit(".", 1)[0] + '.txt'
+        self.image_processor.read_and_draw_annotations(self.annotations_path + '/' + annotation_file_name)
 
         self.pixmap = QPixmap(self.image_processor.convert_to_qpixmap())
         pixmap_width = self.pixmap.width()
@@ -136,7 +160,7 @@ class ImageViewer(QMainWindow):
 
 def main(images_path, annotations_path):
     app = QApplication(sys.argv)
-    viewer = ImageViewer(images_path)
+    viewer = ImageViewer(images_path, annotations_path)
     viewer.show()
     sys.exit(app.exec_())
 
